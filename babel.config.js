f98c57ae7dbebee6fa43b79145cd93d65aa5d712
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2022-10-21 07:42:15 -0300

    feat: create project and install first dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin', ['module-resolver', {
+      alias: {
+        '#Assets': './src/assets',
+        '#Types': './src/@types',
+        '#Components': './src/components',
+        '#Hooks': './src/hooks',
+        '#Routes': './src/routes',
+        '#Screens': './src/screens',
+        '#Services': './src/services',
+        '#Store': './src/store',
+        '#Utils': './src/utils',
+        '#Styles': './src/styles',
+      }
+    }]]
   };
 };
